e2817d0bdc56c5bec96377f7297c2a5e907ee54c
Aldosh <alanshalkey@gmail.com>
2018-08-19 23:04:40 -0600

    final changes

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,36 +1,20 @@
 var friendsArray = [
 {
-    name:"Test",
-    photo:"https://media.licdn.com/dms/image/C5603AQGLkCPC5nU75Q/profile-displayphoto-shrink_200_200/0?e=1540425600&v=beta&t=oh8m3YpITfTPuqSdsLiby-hVwiDwp_YVMaCREnecjOY",
+    name:"Lil Wayne",
+    photo:"http://hw-img.datpiff.com/m5c44b40/Lil_Wayne_The_Blue_Martian_deluxe_Edition-front-large.jpg",
     scores:[
+        4,
         5,
-        1,
         4,
         4,
         5,
-        1,
+        5,
         2,
         5,
-        4,
+        1,
         1
       ]
   },
-  {
-      name:"Test2",
-      photo: "https://media.licdn.com/dms/image/C5603AQGLkCPC5nU75Q/profile-displayphoto-shrink_200_200/0?e=1540425600&v=beta&t=oh8m3YpITfTPuqSdsLiby-hVwiDwp_YVMaCREnecjOY",
-      scores: [
-          1,
-          2,
-          3,
-          4,
-          5,
-          1,
-          2,
-          3,
-          4,
-          5
-      ]
-  }
 ];
 
 module.exports = friendsArray;
